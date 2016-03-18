f1815f90c029e9a5616a606b0e1e77d87a2609aa
Zdyczen <marstyczen@gmail.com>
2016-03-18 14:09:27 +0100

    Logger for starting robot and main() added

---
+++ src/main/java/com/epam/robot/Robot.java
@@ -0,0 +1,13 @@
+package com.epam.robot;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by stycz0007 on 18.03.16.
+ */
+public class Robot {
+    static Logger log = Logger.getLogger(Robot.class.getName());
+    public void start(){
+    log.info("Robot started!");
+    }
+}
